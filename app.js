3b6957f0dabcdd80a04abda187411c4ac8f44fab
odonnellrory <roryod.134@gmail.com>
2024-07-22 21:31:24 +0100

    files set up

---
+++ app.js
@@ -0,0 +1,5 @@
+let playerText = document.getElementById('playerText');
+let restartButton = document.getElementById('restartButton');
+let boxes = Array.from(document.getElementById('box'))
+
+console.log(boxes)
